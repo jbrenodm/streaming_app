cbe0e1e262052082c8cdbc7f7304df8f81a8bdfd
Breno Duarte <jbrenodm@gmail.com>
2020-07-28 16:34:25 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './bushcraftflix_logo.png';
+
+function App() {
+  return (
+    <div>
+      <h1>Texto dentro do "App.js"</h1>
+    </div>
+  );
+}
+
+export default App;
